50a5ad8112fff67038518fd3975e1f8bfa4ae78d
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-15 20:32:15 -0300

    termina "[M1S10] Ex 8 - Adicionando a lógica"

--- devinknowledge-vite/src/contexts/AppContext.jsx
+++ devinknowledge-vite/src/contexts/AppContext.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import { createContext } from "react";
 
@@ -10,11 +10,37 @@ export const useAppContext = () => {
 };
 
 export const AppContext = ({ children }) => {
+  const [allTips, setAllTips] = useState([]);
+  const [filter, setFilter] = useState(null);
+
+  const createTip = (newTip) => {
+    setAllTips((previousTips) => [...previousTips, newTip]);
+  };
+
+  const filterTips = (query) => {
+    setFilter(query ? query : null);
+  };
+
+  const tips =
+    filter === null
+      ? allTips
+      : allTips.filter((tip) => tip.title.includes(filter));
+
   return (
-    <appContext.Provider value={"conectado"}>{children}</appContext.Provider>
+    <appContext.Provider value={{ createTip, filterTips, tips }}>
+      {children}
+    </appContext.Provider>
   );
 };
 
 AppContext.propTypes = {
   children: PropTypes.node,
 };
+
+{
+  titulo: "";
+  categoria: "";
+  linguagem: "";
+  descricao: "";
+  video: "";
+}
